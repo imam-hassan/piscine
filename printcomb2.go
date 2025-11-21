c52fb3ab3b312b898fab55f6937b7cae794f5a29
imam-hassan <imamhassanadanla@gmail.com>
2025-11-21 14:40:52 +0000

    printcomb2

---
+++ printcomb2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func PrintComb2() {
+	for i := 0; i <= 98; i++ {
+		for j := i + 1; j <= 99; j++ {
+			fmt.Printf("%02d %02d", i, j)
+			if !(i == 98 && j == 99) {
+				fmt.Print(", ")
+			}
+		}
+	}
+	fmt.Println()
+}
+
+func main() {
+	PrintComb2()
+}
