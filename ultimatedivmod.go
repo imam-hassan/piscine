1097141ac890c66e0a998379bb7d265afbfe3257
imam-hassan <imamhassanadanla@gmail.com>
2025-11-21 15:40:58 +0000

    ultimatedivmod

---
+++ ultimatedivmod.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func UltimateDivMod(a *int, b *int) {
+  div := *a/ *b
+  mod := *a% *b
+  *a = div
+  *b = mod
+}
+
+func main() {
+	a := 13
+	b := 2
+	UltimateDivMod(&a, &b)
+	fmt.Println(a)
+	fmt.Println(b)
+}
