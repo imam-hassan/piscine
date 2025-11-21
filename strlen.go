c48ab0deae43d35a5f1129076ad65723d6babbf5
imam-hassan <imamhassanadanla@gmail.com>
2025-11-21 15:48:55 +0000

    strlen

---
+++ strlen.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func StrLen(s string) int {
+    count := 0
+    
+	for range s {
+		count ++
+	}
+	return count
+}
+
+func main() {
+	l := StrLen("Hello World!")
+	fmt.Println(l)
+}
