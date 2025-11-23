67c4f63292a928e0d47b3e343c4d13569353cc31
imam-hassan <imamhassanadanla@gmail.com>
2025-11-23 16:51:10 +0000

    fibonacci

---
+++ fibonacci.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func Fibonacci(index int) int {
+  if index == 0{
+	return 0
+  }
+  if index == 1{
+	return 1
+  }
+  return Fibonacci(index-1) + Fibonacci(index-2)
+
+}
+
+func main() {
+	arg1 := 4
+	fmt.Println(Fibonacci(arg1))
+}
