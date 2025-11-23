5b31a8c936a6934b3ba9c3772c2bc44d612c7569
imam-hassan <imamhassanadanla@gmail.com>
2025-11-23 15:11:43 +0000

    recursivepower

---
+++ recursivepower.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func RecursivePower(nb int, power int) int {
+   if power < 0{
+	return 0
+   }
+   if power == 0{
+	return 1
+   }
+   return nb * RecursivePower(nb,power-1)
+}
+
+func main() {
+	fmt.Println(RecursivePower(4, 3))
+}
