99f734958fb31f1f10468c503ea22ec9420298a3
imam-hassan <imamhassanadanla@gmail.com>
2025-11-28 18:47:57 +0000

    compare

---
+++ compare.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func Compare(a, b string) int {
+     if a < b {
+		return -1
+	 }else if a > b {
+		return 1
+	 }
+	 return 0
+}
+
+func main() {
+	fmt.Println(Compare("Hello!", "Hello!"))
+	fmt.Println(Compare("Salut!", "lut!"))
+	fmt.Println(Compare("Ola!", "Ol"))
+}
