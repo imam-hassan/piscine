a9de3418b2db18288bfc22f4126e43ec795c28d0
imam-hassan <imamhassanadanla@gmail.com>
2025-11-29 15:23:55 +0000

    isupper

---
+++ isupper.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func IsUpper(s string) bool {
+  for _, char := range s{
+    if char < 'A' || char > 'Z'{
+		return false
+	} 
+  }
+  return true
+}
+
+
+func main() {
+	fmt.Println(IsUpper("HELLO"))
+	fmt.Println(IsUpper("HELLO!"))
+
+}
